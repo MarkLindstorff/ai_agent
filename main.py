ae0531509436eac4bfc563b251164bc2edef1780
MarkLindstorff <MLAGit@protonmail.com>
2026-02-12 12:02:42 +0100

    Calculator app

--- main.py
+++ main.py
@@ -4,17 +4,12 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-
-
 load_dotenv()
 
 api_key = os.environ.get("GEMINI_API_KEY")
 if api_key == None:
     raise RuntimeError("API wasnt found")
 client = genai.Client(api_key=api_key)
-
-
-
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
@@ -36,10 +31,8 @@ def main():
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
     
     print(f"Response:\n{response.text}")
-
 
 if __name__ == "__main__":
     main()
