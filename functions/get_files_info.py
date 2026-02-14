796367234642bf908faca57245744a98b90d2366
MarkLindstorff <MLAGit@protonmail.com>
2026-02-14 11:19:10 +0100

    safeguards

---
+++ functions/get_files_info.py
@@ -0,0 +1,27 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+
+        if valid_target_dir == False:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+
+        items = []
+
+        for name in os.listdir(target_dir):
+            item_path = os.path.join(target_dir, name)
+            item_size = os.path.getsize(item_path)
+            item_isdir = os.path.isdir(item_path)
+
+            answer_str = f"- {name}: file_size={item_size} bytes, is_dir={item_isdir}"
+            items.append(answer_str)
+
+        return "\n".join(items)
+    except Exception as e:
+        return f"Error: {e}"
